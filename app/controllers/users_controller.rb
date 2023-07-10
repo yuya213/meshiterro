3d75d245b0bcf0f01d9cf2b91ed7b926ffebdd7f
yuya213 <129849154+yuya213@users.noreply.github.com@users.noreply.github.com>
2023-07-10 14:35:04 +0000

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
